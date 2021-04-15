58a80f2ba6d68699eb22e69224bc75aaf20e03dd
Risa Igarashi <risa.igarashi@webcrew.co.jp>
2021-04-15 11:48:02 +0900

    add: とりあえずSCSS

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,9 +5,6 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'step_ui_ts',
-    htmlAttrs: {
-      lang: 'en'
-    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -20,6 +17,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    { src: '@/assets/scss/_reset.scss', lang: 'scss' },
+    { src: '@/assets/scss/_common.scss', lang: 'scss' },
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -45,6 +44,12 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
+
+  styleResources: {
+    scss: [
+      '@/assets/scss/_common.scss'
+    ]
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
