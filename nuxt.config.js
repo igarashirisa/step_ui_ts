8bdb31240305774828effdd24aa001374b3742de
Risa Igarashi <risa.igarashi@webcrew.co.jp>
2021-04-15 15:15:20 +0900

    mod:STEPのCSSを調整

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'step_ui_ts',
+    title: 'STEP UIサンプル集',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,53 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        step_ui_ts
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div id="main">
+    <div class="form">
+      <h1 class="form_title">STEP UI</h1>
+      <div class="form_inner">
+        <div class="form_item">
+          <div class="form_subTitle">
+            フリガナのUI
+          </div>
+          <div class="form_input">
+            <input type="text" name="kana" placeholder="フリガナ" id="kana">
+          </div>
+        </div>
+        <p class="form_text"></p>
+      </div>
+      <div class="form_inner">
+        <div class="form_item">
+          <div class="form_subTitle">
+            郵便番号のUI
+          </div>
+          <div class="form_input">
+            <input type="tel" name="zip" placeholder="郵便番号" id="zip" maxlength="8">
+          </div>
+        </div>
+        <p class="form_text">
+          主に郵便番号で使えるUI。<br>
+          全角を半角へ変換。ハイフンを取り除く。
+        </p>
+      </div>
+      <div class="form_inner">
+        <div class="form_item">
+          <div class="form_subTitle">
+            電話番号のUI
+          </div>
+          <div class="form_input">
+            <input type="tel" name="tel" placeholder="電話番号" id="tel" maxlength="13">
+          </div>
+        </div>
+        <p class="form_text"></p>
+      </div>
+      <div class="form_inner">
+        <div class="form_item">
+          <div class="form_subTitle">
+            メールアドレスのUI
+          </div>
+          <div class="form_input">
+            <input type="text" name="mail" placeholder="メールアドレス" id="mail">
+          </div>
+        </div>
+        <p class="form_text"></p>
       </div>
     </div>
   </div>
@@ -33,43 +59,52 @@ import Vue from 'vue'
 export default Vue.extend({})
 </script>
 
-<style>
-.container {
+<style lang="scss">
+#main {
   margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  padding: 20px 0;
+  width: 1020px;
+
+  .form {
+    &_title {
+      margin-bottom: 15px;
+      padding: 13px 15px;
+      background-color: #2a65b7;
+      border-radius: 3px;
+      line-height: 1;
+      font-size: 163%;
+      font-weight: 700;
+      color: #fff;
+    }
+
+    &_inner {
+      margin: 0 auto 20px;
+      border: 4px solid #ddd;
+      border-radius: 3px;
+      overflow: hidden;
+      width: 860px;
+      background-color: #fff;
+    }
+
+    &_subTitle {
+      padding: 3px 0;
+      width: 270px;
+      font-weight: 600;
+      color: #333;
+      box-sizing: border-box;
+    }
+
+    &_item {
+      margin-top: 10px;
+      padding: 10px 20px;
+      display: flex;
+      border-bottom: dotted 1px #777;
+    }
+
+    &_text {
+      padding: 15px 20px;
+    }
+  }
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
